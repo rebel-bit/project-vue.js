06cd06a32d2455201283a9a7ed1f6143918e140d
malaFillatunnida <fillatunnida@gmail.com>
2021-11-09 12:27:13 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,203 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+     <div class="mobile-navigasi">
+            <div class="mobile-navigasi-animasipindah"></div>
+            <div class="mobile-navigasi-item active">
+                <a href="#home">
+                <i class="bx bxs-home"></i>
+                </a>
+            </div>
+            <div class="mobile-navigasi-item">
+                <a href="#kontener">
+                <i class='bx bxs-food-menu'></i>
+                </a>
+            </div>
+            <div class="mobile-navigasi-item">
+                <a href="#tentang">
+                <i class='bx bxs-wink-smile'></i>
+                </a>
+            </div>
+            <div class="mobile-navigasi-item">
+                <a href="#footer">
+                <i style='color: silver;' class='bx bxs-comment-detail'></i>
+                </a>
+            </div>
+            <div class="mobile-navigasi-item">
+                <a href="#info">
+                <i style='color: silver;' class='fa fa-info'></i>
+                </a>
+            </div>
+        </div>
+        <a href="#home" class="back-to-top">
+        <i class="bx bxs-to-top"></i>
+        </a>
+        <div class="nav">
+            <div class="menu-wrap">
+                <a href="#home">
+                    <div class="logo" style="color: black;">
+                        perusahan motor
+                    </div>
+                </a>
+                <div class="menu h-xs">
+                    <a href="#home">
+                        <div class="menu-item active">
+                            VISI
+                        </div>
+                    </a>
+                    <a href="#kontener">
+                        <div class="menu-item">
+                            MISI
+                        </div>
+                    </a>
+                     <a href="#sambutan">
+                        <div class="menu-item">
+                            Sambutan
+                        </div>
+                    </a>
+                    <a href="#tentang">
+                        <div class="menu-item">
+                            Member
+                        </div>
+                    </a>
+                    <a href="#info">
+                        <div class="menu-item">
+                            Pelangan
+                        </div>
+                    </a>
+                    <a href="#footer">
+                        <div class="menu-item">
+                            Hubungi Saya
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+        
+        <section id="home" class="align-items-center bekgron-gambar bekgron-gambar-fixed" style="background-image: url(assets/begeh2.jpg);">
+             <div class="container">
+                <div class="kontener">
+                    <h1>
+                        VISI KAMI
+                    </h1>
+                    <p>Kami senantiasa berusaha Menjadi yang terbaik dalam bisnis Perdagangan Sepeda Motor serta suku cadang dan aksesorisnya sesuai kebutuhan konsumen serta didukung oleh Fasilitas Terpadu, jaringan pemeliharaan, suku cadang dan Management yang Profesional.</p>
+                </div>
+            </div>
+        </section>
+        <section id="kontener" class="align-items-center bekgron-gambar bekgron-gambar-fixed"  style="background-image: url(assets/begeh2.jpg);">
+             <div class="container">
+                <div class="kontener">
+                    <h1>
+                        MISI KAMI 
+                    </h1>
+                    <p>Kami bertekad untuk memberikan pelayanan yang terbaik bagi konsumen dengan pelayanan yang memuaskan dan Profesional</p>
+                </div>
+            </div>
+        </section>
+
+        <div class="slider">
+		<figure>
+			<div class="slide">
+				<img src="images/slide_one.jpg">
+			</div>
+
+			<div class="slide">
+				<img src="images/slide_two.jpg">
+			</div>
+
+			<div class="slide">
+				<img src="images/slide_three.jpg">
+			</div>
+
+			<div class="slide">
+				<img src="images/slide_four.jpg">
+			</div>
+
+			<div class="slide">
+				<img src="images/slide_five.jpg">
+			</div>
+		</figure>
+	</div>
+
+          <section id="sambutan" class="align-items-center bekgron-gambar bekgron-gambar-fixed"  style="background-image: url(assets/begeh2.jpg);">
+             <div class="container">
+                <div class="kontener">
+                    <h1>
+                        Selamat Datang Di deller Motor Maju Jaya
+                    </h1>
+                    <p>Kami Di sini siap memberikan pelayanan yang terbaik bagi konsumen dengan pelayanan yang sangat memuaskan dan Profesional <br>Dan kami siap memberikan Pelayanan 24 jam kepada kosumen </p>
+                </div>
+            </div>
+        </section>
+
+        <section id="tentang" class="align-items-center bekgron-gambar bekgron-gambar-fixed"  style="background-image: url(assets/begeh2.jpg);">
+             <div class="container">
+                <div class="kontener">
+                    <h1>
+                        Ceo 
+                    </h1>
+                    <p>Kami bertekad untuk memberikan pelayanan yang terbaik bagi konsumen dengan pelayanan yang memuaskan dan Profesional</p>
+                </div>
+            </div>
+            <div class="container">
+                <div class="kontener">
+                    <h1>
+                        founder in leader
+                    </h1>
+                    <p>Kami bertekad untuk memberikan pelayanan yang terbaik bagi konsumen dengan pelayanan yang memuaskan dan Profesional</p>
+                </div>
+            </div>
+            <div class="container">
+                <div class="kontener">
+                    <h1>
+                        founder in leader
+                    </h1>
+                    <p>Kami bertekad untuk memberikan pelayanan yang terbaik bagi konsumen dengan pelayanan yang memuaskan dan Profesional</p>
+                </div>
+            </div>
+        </section>
+        
+         <section id="info" class="align-items-center bekgron-gambar bekgron-gambar-fixed"  style="background-image: url(assets/begeh2.jpg);">
+             <div class="container">
+                <div class="kontener">
+                <h1>Pelangan</h1>
+                     <p>Terima kasih kepada pelanggan Anda karena telah mengeluh dan bersungguh-sungguh. Sebagian besar tidak akan pernah repot-repot mengeluh. Mereka akan pergi begitu saja." - Marilyn Suttle</p>
+                 </div>
+            </div>
+        </section>
+
+        
+        
+        <section class="footer bekgron-gambar" id="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-6 col-xs-12">
+                        <h1>
+                            
+                        </h1>
+                        <br>
+                        <p>Mari buat bisnis / usaha Anda selangkah lebih maju. Hubungi Saya Sekarang!</p>
+                        <br>
+                        <p><i class="fa fa-envelope-square"></i> Email&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: <a href="mailto:https://xcodeteam6@gmail.com">xcodeteam6@gmail.com</a></p>
+                        <p><i class="fa fa-facebook-square"></i> Facebook&nbsp;&nbsp;:xcode_12 </p>
+                        <p><i class="fa fa-instagram"></i> Instagram    :@xcode_12 </p>
+                        <p><i class="fa fa-code"></i>Website&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: <a href="https://google.com">google.com</a></p>
+                        <p><i class="fa fa-whatsapp"></i> whatsapp&nbsp;&nbsp;:<a href="https://wa.me/6289510104497?text=assalamulaikum">+6289510104497</a></p>
+                    </div>
+                    <div class="col-6 col-xs-12">
+                        <h1 style="color: black;">
+                            Hubungi Saya
+                        </h1>
+                        <br>
+                        <p>Saya selalu menunggu Anda, Mari mulai sedikit kemajuan bersama Saya untuk bisnis / usaha Anda!</p>
+                        <div class="input-group">
+                            <input type="text" placeholder="Masukan Alamat Email Anda">
+                            <button style="background-color: black;">
+                                   Hubungi
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
